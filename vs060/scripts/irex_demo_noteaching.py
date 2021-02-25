7cd07a94a2e1c1cf0db7432c3ff183754204a789
infi <infixw@gmail.com>
2021-02-25 16:16:05 +0800

    update

--- vs060/scripts/irex_demo_noteaching.py
+++ vs060/scripts/irex_demo_noteaching.py
@@ -34,61 +34,64 @@
 #
 # Author: Isaac Isao Saito
 
-## THIS IS A COPY FROM irex_demo.py, removing reading dead-man switch from pendant
+# THIS IS A COPY FROM irex_demo.py, removing reading dead-man switch from pendant
 
-#%Tag(FULLTEXT)%
+# %Tag(FULLTEXT)%
 
-## workaround until https://github.com/ros-planning/moveit/pull/581 is released
+# workaround until https://github.com/ros-planning/moveit/pull/581 is released
+from tf.transformations import quaternion_from_euler
+import std_msgs.msg
+import rospy
+import roslib
+from rospkg import RosPack
+from moveit_commander import MoveGroupCommander, conversions
+from geometry_msgs.msg import Pose, PoseStamped, Point, Quaternion
+import actionlib_msgs.msg
+from subprocess import check_call
+import os
+import pyassimp
 import sys
 sys.modules["pyassimp"] = sys
-import pyassimp
 
 
-import os
-from subprocess import check_call
-
-import actionlib_msgs.msg
-from geometry_msgs.msg import Pose, PoseStamped, Point, Quaternion
-from moveit_commander import MoveGroupCommander, conversions
-from rospkg import RosPack
-import roslib
-import rospy
-import std_msgs.msg
-from tf.transformations import quaternion_from_euler
-#roslib.load_manifest("denso_pendant_publisher")
-#roslib.load_manifest("actionlib_msgs")
+# roslib.load_manifest("denso_pendant_publisher")
+# roslib.load_manifest("actionlib_msgs")
 
 rospy.init_node("test_vs060_moveit")
 
 arm = MoveGroupCommander("manipulator")
-running_pub = rospy.Publisher("/irex_demo_running", std_msgs.msg.Bool);
+running_pub = rospy.Publisher(
+    "/irex_demo_running", std_msgs.msg.Bool, queue_size=10)
 #cancel_pub = rospy.Publisher("/arm_controller/follow_joint_trajectory/cancel", actionlib_msgs.msg.GoalID);
-cancel_pub = rospy.Publisher("/move_group/cancel", actionlib_msgs.msg.GoalID);
+cancel_pub = rospy.Publisher(
+    "/move_group/cancel", actionlib_msgs.msg.GoalID, queue_size=10)
 
 # Get paths for files used later.
 _rospack = RosPack()
 SCENE_FILE = _rospack.get_path('vs060') + '/model/irex_model.scene'
 _path_rosroot = rospy.get_ros_root()
-_len_cut = len(_path_rosroot) - len('/share/ros')  # This is used to get ros root path /opt/%DISTRO% path.
+# This is used to get ros root path /opt/%DISTRO% path.
+_len_cut = len(_path_rosroot) - len('/share/ros')
 _path_rosroot_top = _path_rosroot[:_len_cut]
 LOAD_SCENE_PROG = _path_rosroot_top + '/lib/vs060/publish_scene_from_text'
 
 print 'SCENE_FILE=', SCENE_FILE
 print 'LOAD_SCENE_PROG=', LOAD_SCENE_PROG
 
-def demo() :
+
+def demo():
     # load scene
     check_call([LOAD_SCENE_PROG, SCENE_FILE])
-    for p in [[ 0.35, -0.35, 0.4],
-              [ 0.6,  0.0, 0.4],
-              [ 0.35,  0.35, 0.4],
-              [ 0.6,  0.0, 0.2],
-              [ 0.4,  0.0, 0.8]]:
+    for p in [[0.35, -0.35, 0.4],
+              [0.6,  0.0, 0.4],
+              [0.35,  0.35, 0.4],
+              [0.6,  0.0, 0.2],
+              [0.4,  0.0, 0.8]]:
         if True:
             print "set_pose_target(", p, ")"
-            pose = PoseStamped(header = rospy.Header(stamp = rospy.Time.now(), frame_id = '/BASE'),
-                               pose = Pose(position = Point(*p),
-                                           orientation = Quaternion(*quaternion_from_euler(1.57, 0, 1.57, 'sxyz'))))
+            pose = PoseStamped(header=rospy.Header(stamp=rospy.Time.now(), frame_id='/BASE'),
+                               pose=Pose(position=Point(*p),
+                                         orientation=Quaternion(*quaternion_from_euler(1.57, 0, 1.57, 'sxyz'))))
 
             arm.set_pose_target(pose)
             arm.go() or arm.go() or rospy.logerr("arm.go fails")
@@ -96,7 +99,8 @@ def demo() :
             if rospy.is_shutdown():
                 return
 
+
 if __name__ == "__main__":
     while not rospy.is_shutdown():
         demo()
-#%EndTag(FULLTEXT)%
+# %EndTag(FULLTEXT)%
